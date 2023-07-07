0cb0bd77eae04971cdc67ef86130a0b06d8a2af4
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-07-07 12:02:08 -0300

    google1

--- musicas/settings.py
+++ musicas/settings.py
@@ -74,21 +74,14 @@ WSGI_APPLICATION = 'musicas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'anderson',
+        'PASSWORD': 'sosa1808',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django_db',
-#         'USER': 'anderson',
-#         'PASSWORD': 'sosa1808',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation
