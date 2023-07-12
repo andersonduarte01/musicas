01e5cab3a1b455a65870be5ba044c82b0f61fe47
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-07-12 12:14:34 -0300

    show

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from ..album.models import Album
 
 
 # Create your views here.
 
 
-class Index(TemplateView):
+class Index(ListView):
+    model = Album
     template_name = 'core/home.html'
+    context_object_name = 'albuns'
+
+    def get_queryset(self):
+        return Album.objects.filter().order_by('-atualizado')[:7]
+
 
 
